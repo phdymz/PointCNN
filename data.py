b59564ca23288612878870d1d14e5baab5e1c578
ymz <mzyuan20@fudan.edu.cn>
2022-08-10 16:56:23 +0800

    data

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ from model import PointCNN
 from torch_geometric.datasets import ModelNet
 import torch_geometric.transforms as T
 import os.path as osp
-
+from torch.utils.data import Dataset
 
 
 def get_dataset(num_points):
@@ -38,6 +38,72 @@ def voxel2pcd(img, threshold = 0.1):
     pcd = img_masked.nonzero()
 
     return pcd
+
+
+
+
+class Vessel(Dataset):
+    def __init__(self, num_points = None, phase = 'train'):
+        self.gaussian_noise = 0.01
+        self.phase = phase
+        self.num_points = num_points
+
+    def __getitem__(self, item):
+
+        if self.phase == 'train':
+            pcd = jitter_pointcloud(pcd, self.gaussian_noise)
+
+        if not self.num_points == None:
+            np.random.shuffle(pcd)
+            pcd = pcd[:self.num_points]
+        return pcd
+
+    def __len__(self):
+        return len()
+
+    def jitter_pointcloud(self, pointcloud, sigma=0.01, clip=0.01):
+        N, C = pointcloud.shape
+        pointcloud += np.clip(sigma * np.random.randn(N, C), -1 * clip, clip)
+        return pointcloud
+
+
+
+def collate_fn_vessel(list_data):
+    pos = []
+    batch = []
+    for ind, pcd in enumerate(list_data):
+        pos.append(pcd)
+        batch.append(np.ones(len(pcd)))
+
+    batch = np.vstack(batch)
+    pos = np.array(pos)
+
+    return torch.from_numpy(pos), torch.from_numpy(batch)
+
+
+
+
+
+
+if __name__ == "__main__":
+    dataset = Vessel()
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=2,
+        shuffle=True,
+        num_workers=8,
+        collate_fn=collate_fn_vessel,
+        drop_last=False
+    )
+
+
+
+
+
+
+
+
+
 
 
 
